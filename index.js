b91b8ee930a2d24983df565673f5f7e46ed4785c
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-27 22:07:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 const returnFirstTwoDrivers = function (drivers) {
     return [drivers[0], drivers[1]];
 }
+
+const returnLastTwoDrivers = function(drivers){
+  return [drivers[-2], driver[-1]];
+}
