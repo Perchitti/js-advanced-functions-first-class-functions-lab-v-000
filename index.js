72cf56c72b38cf8bb4a3594a3e4cdd019d0ffbe1
Lauren Perchitti <laurenperchitti@gmail.com>
2018-06-27 22:02:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(drivers){
-  return drivers.map(function(driver){
+  return drivers.filter(function(driver){
     return driver[0] && driver[1]
   })
 }
